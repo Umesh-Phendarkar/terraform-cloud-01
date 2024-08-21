4fa0690b851a7e007c7325796efd437b85d7db92
Umesh-Phendarkar <umesh.phendarkar@gmail.com>
2024-08-21 08:50:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,19 @@
 provider "aws" {
   region = "ap-south-1"
 }
+
+
+terraform {
+  cloud {
+    organization = "umesh01"
+    workspaces {
+      name = "workspace-01"
+    }
+  }
+
+}
+
+
 resource "aws_s3_bucket" "mys3bucket10" {
   bucket = "my-s3-bucket-03-11-2024"
   acl    = "private"
